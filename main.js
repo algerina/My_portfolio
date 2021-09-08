91fc1583c3f372bd21024315fbdfd7f927ab9f87
Amel Khiri <53347966+algerina@users.noreply.github.com>
2021-09-08 20:38:28 +0100

    Fix popup image

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ seeproject4.addEventListener('click', () => {
 const cardArray = [{
   header: 'Tonic',
   list: ['Canopy', 'Back End Dev', '2015'],
-  image: '../img/popupdesk.png',
+  image: './img/popupdesk.png',
   text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s',
   listbuttons: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstrap'],
   live: 'https://algerina.github.io/My_portfolio/',
