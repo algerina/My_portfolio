5ac09c2ea2b9d812f65b0b5421fdb18823788096
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-09-08 20:29:50 +0300

    apply JavaScript to open the window

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const hambuMenu = document.querySelector('.hambu-menu');
 const closeMenu = document.querySelector('.x-img');
 const menuToggle = document.querySelector('#menu-toggle');
 const hambuChilds = document.querySelectorAll('.hambu-child');
+const seeproject = document.querySelector('.big-buttonds');
+const popupWindow = document.querySelector('#popup');
 
 function show() {
   hambuMenu.style.display = 'flex';
@@ -17,22 +19,27 @@ hambuChilds.forEach((chld) => {
   chld.addEventListener('click', close);
 });
 
-const cardArray = [ {
-    header: 'Tonic',
-    list: ['Canopy', 'Back End Dev', '2015'],
-    image: '../img/popupdesk.png',
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s',
-    listbuttons: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstrap'], 
-    live: 'https://algerina.github.io/My_portfolio/', 
-    source: 'https://github.com/algerina', 
+seeproject.addEventListener('click', () => {
+  popupWindow.classList.add('popup-active');
+});
+
+const cardArray = [{
+  header: 'Tonic',
+  list: ['Canopy', 'Back End Dev', '2015'],
+  image: '../img/popupdesk.png',
+  text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s',
+  listbuttons: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstrap'],
+  live: 'https://algerina.github.io/My_portfolio/',
+  source: 'https://github.com/algerina',
 
 },
 
-]
+];
 
 const popup = document.getElementById('popup');
 cardArray.forEach((item) => {
   popup.innerHTML += `
+  <span class="close">X</span>
 <h2 class="popup-name">
 ${item.header}
 </h2>
