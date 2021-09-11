f0cfab89abe6732bde96f2b2970f766a3d56421d
Amel Khiri <53347966+algerina@users.noreply.github.com>
2021-09-11 01:47:28 +0100

    Fix Array
    Fix Array

---
+++ test.js
@@ -0,0 +1,18 @@
+const cardsArray = [{
+    header: 'Tonic',
+    list: ['Canopy', 'Back End Dev', '2015'],
+    image: './img/popupdesk.png',
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s',
+    listbuttons: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstrap'],
+    live: 'https://algerina.github.io/My_portfolio/',
+    source: 'https://github.com/algerina',
+  
+  },
+  {
+      header:'Multi-Post Stories'
+      list: ['Facebook', 'Full Stack Dev', '2015'],
+      text: ''
+
+  }
+  
+  ];
