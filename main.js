05f981788c0ac32df1ed3b59f91a1df3628bb15a
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-08-26 21:40:11 +0100

    Add dropdown list

--- main.js
+++ main.js
@@ -176,11 +176,3 @@ const populatePopup = (num) => {
   })
 }
 
-function myFunction() {
-    var x = document.getElementById("Demo");
-    if (x.className.indexOf("w3-show") == -1) { 
-      x.className += " w3-show";
-    } else {
-      x.className = x.className.replace(" w3-show", "");
-    }
-  }
